ca89c8e800cde8c51e3e004aadc0cce68ae50247
Lavio Vale <laviovale@gmail.com>
2021-11-25 11:57:04 -0300

    testing

--- firebase/saveData.js
+++ firebase/saveData.js
@@ -1,6 +1,6 @@
 const firebase = require('./firebase_connect');
 
-module.exports = {
+/* module.exports = {
     saveData: function(req, callback){
         firebase.database().ref(`${req.cep}`).set({
             cep: req.cep,
@@ -10,6 +10,15 @@ module.exports = {
             uf: req.uf,
             complemento: req.complemento,
             ddd: req.ddd,
+        })
+        callback(null, {"statuscode": 200, "message": "Data inserted successfully"});
+    }
+} */
+
+module.exports = {
+    saveData: function(req, callback){
+        firebase.database().ref("teste").set({
+            "teste": 123,
         })
         callback(null, {"statuscode": 200, "message": "Data inserted successfully"});
     }
