a54e1a3e2e07439612b4b39ab25b98dcac0d3bd6
Lavio Vale <laviovale@gmail.com>
2021-11-24 22:22:53 -0300

    teste

--- server.js
+++ server.js
@@ -8,21 +8,21 @@ const insert = require('./firebase/insertData')
 const port = process.env.PORT || 5000;
 
 app.use(express.json())
-app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 
 app.get('/api/:cep', async (req, res) => {
-    fetch(`https://viacep.com.br/ws/${req.params.cep}/json/`, { mode: 'cors' })
+    fetch(`https://viacep.com.br/ws/${req.params.cep}/json/`)
     .then(response => response.json())
     .then(data => res.send(data))
     console.log('called')
 });
-
-app.listen(port, () => console.log(`Server running on port ${port}`));
 
 app.post('/firebase/insert/', (req) => {
     insert.saveData(req.body, function(err, data){
         res.send(data)
     });
 })
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
