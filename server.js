03ae72b595e4c4c8f9f9dcbbd6f9d7d8ec9583eb
Lávio Vale <44332001+laviodias@users.noreply.github.com>
2021-11-25 11:09:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fetch = require('cross-fetch');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-//const insert = require('./firebase/insertData')
+const insert = require('./firebase/insertData')
 
 const port = process.env.PORT || 5000;
 
