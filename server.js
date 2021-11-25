afd0ad02f69ebd6374a0ca754c8adf7df04ecc7b
Lavio Vale <laviovale@gmail.com>
2021-11-24 21:33:32 -0300

    add cors 3

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const app = express();
 const fetch = require('cross-fetch');
-const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
-
 app.use(express.json())
+app.use(cors())
 
 app.get('/api/:cep', async (req, res) => {
     fetch(`https://viacep.com.br/ws/${req.params.cep}/json/`, { mode: 'cors' })
