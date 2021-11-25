b7eef45f1d19f66ca25b7cebc3daa770a26d15cf
Lavio Vale <laviovale@gmail.com>
2021-11-25 12:00:18 -0300

    removing firebase

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const fetch = require('cross-fetch');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const ofirebase = require('./firebase/saveData');
 
 const port = process.env.PORT || 5000;
 
@@ -21,20 +20,6 @@ app.get('/api/:cep', async (req, res) => {
     const response = await fetch(`https://viacep.com.br/ws/${req.params.cep}/json/`)
     const data = await response.json()
     res.send(data)
-});
-
-app.post('/firebase/insert/', (req, res) => {
-    ofirebase.saveData({
-        cep: req.body.cep,
-        localidade: req.body.localidade,
-        logradouro: req.body.logradouro,
-        bairro: req.body.bairro,
-        uf: req.body.uf,
-        complemento: req.body.complemento,
-        ddd: req.body.ddd,
-    }, function(err, data){
-        res.send(data);
-    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
