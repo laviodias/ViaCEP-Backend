bdf3eb76956e0343844c5f10d8bf2e7180a86682
Lavio Vale <laviovale@gmail.com>
2021-11-24 23:01:42 -0300

    ...

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://lavio-viacep.netlify.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET, POST");
     app.use(cors());
     next();
 });
