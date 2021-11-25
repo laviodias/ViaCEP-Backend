caaa1c1c5de50f54053cc05239d843943d3024ab
Lavio Vale <laviovale@gmail.com>
2021-11-24 21:29:21 -0300

    add cors 2

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const cors = require('cors');
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static('./viaCep/src'))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 app.use(express.json())
 
 app.get('/api/:cep', async (req, res) => {
-    res.set('Access-Control-Allow-Origin', '*');
     fetch(`https://viacep.com.br/ws/${req.params.cep}/json/`, { mode: 'cors' })
     .then(response => response.json())
     .then(data => res.send(data))
