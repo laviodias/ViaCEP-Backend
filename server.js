eb9e92c4d63cd33917c3887a7699a32b225bb00b
Lávio Vale <44332001+laviodias@users.noreply.github.com>
2021-11-25 11:02:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,21 @@ const express = require('express');
 const fetch = require('cross-fetch');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const insert = require('./firebase/insertData')
+//const insert = require('./firebase/insertData')
 
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    app.use(cors());
+    next();
+});
 
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-/* app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    app.use(cors());
-    next();
-});*/
 
 app.get('/api/:cep', async (req, res) => {
     fetch(`https://viacep.com.br/ws/${req.params.cep}/json/`)
